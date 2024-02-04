5b6b50bd7f3679ee882f4ec568fa8d8315665d42
Vedant Yengupatla <vedantyengupatla1235@gmail.com>
2024-02-04 14:09:11 +0530

    Added Mobile Menu Functionality

--- try.js
+++ try.js
@@ -1,3 +1,27 @@
 // Checking Msg
 
 console.log("Welcome To Classify");
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar'); // Replace 'navbar' with the actual ID of your navbar element
+    if (window.scrollY > 20) { // Change 20 to the desired scroll threshold
+        navbar.style.backgroundColor = 'black'; // Change navbar background color to black
+    } else {
+        navbar.style.backgroundColor = ''; // Revert back to default background color when not scrolled 20px
+    }
+});
+
+//* Mobile Menu
+
+let menuBtn = document.getElementById("menuBtn");
+menuBtn.addEventListener("click",function(){
+    // console.log("You Cliked On MenuBtn");
+    let mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.style.transform = "translateX(0px)";
+})
+
+let closeMenu = document.getElementById("closeMenu");
+closeMenu.addEventListener("click",function(){
+    let mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.style.transform = "translateX(1100px)";
+})
